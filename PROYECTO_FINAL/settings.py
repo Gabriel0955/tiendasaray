02baa06790dd82fcd00e12511087c0f14e5e00f4
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2023-08-14 21:43:34 -0500

    Update settings.py

--- PROYECTO_FINAL/settings.py
+++ PROYECTO_FINAL/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%o()_ccauh)n0prd_-pbe-!-egnkmurzq^_r4am!6i^kwkbme8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tiendasaray.onrender.com']
 
 
 # Application definition
@@ -106,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Guayaquil'
 
 USE_I18N = True
 
