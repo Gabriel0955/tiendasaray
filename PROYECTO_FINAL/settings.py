4b85f18cc174c61bd54de21e7ff665e9c21d6743
Gabriel Aguirre <108961289+Gabriel0955@users.noreply.github.com>
2023-08-14 21:32:42 -0500

    Update settings.py

--- PROYECTO_FINAL/settings.py
+++ PROYECTO_FINAL/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%o()_ccauh)n0prd_-pbe-!-egnkmurzq^_r4am!6i^kwkbme8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tiendasaray.onrender.com']
 
 
 # Application definition
